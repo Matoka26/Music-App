8a6f6946c68cd616135e4a79a06757574ee0545d
Mihai <mihail_danut24@yahoo.com>
2024-04-08 19:27:44 +0300

    started repos

---
+++ src/Persistence/Repositories/App_UserRepository.java
@@ -0,0 +1,8 @@
+package Persistence.Repositories;
+
+import Models.APP_User.App_User;
+import Persistence.GenericRepository;
+
+public class App_UserRepository implements GenericRepository<App_User> {
+    
+}
