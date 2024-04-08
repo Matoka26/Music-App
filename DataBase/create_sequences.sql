f46c7ce686a9842b6a35ff131fcdb3788e7b0202
Mihai <mihail_danut24@yahoo.com>
2024-04-08 15:50:49 +0300

    initial commit,added DB

---
+++ DataBase/create_sequences.sql
@@ -0,0 +1,66 @@
+create SEQUENCE user_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE artist_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE listener_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE track_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE album_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE podcast_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE episode_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999
+    NOCACHE
+    NOCYCLE;
+    
+create SEQUENCE song_index
+    START WITH 1
+    INCREMENT BY 1
+    MAXVALUE 999999999
+    NOCACHE
+    NOCYCLE;
+    
+
+
+    
+
+
+    
+
+    
+
+    
